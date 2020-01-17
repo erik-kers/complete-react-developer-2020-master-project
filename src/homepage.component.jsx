094992a1a86844dbb6bef7bf97aa1ea39897f56c
Erik Kers <erik.kers.ohrnell@gmail.com>
2020-01-17 15:58:10 +0100

    Create homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Jackets</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Sneakers</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Womens</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Mens</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-meny'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <span className='subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default HomePage;

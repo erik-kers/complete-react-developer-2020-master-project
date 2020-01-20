0a80cb7632873eb7d4c29777f7a5426ce27e8901
Erik Kers <erik.kers.ohrnell@gmail.com>
2020-01-20 20:57:41 +0100

    Add Sign In page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './page/homepage/homepage.component';
 import { Switch, Route} from 'react-router-dom';
 import ShopPage from './page/shop/shop.component';
+import SignInAndSignUp from './page/sign-in-and-sign-out/sign-in-and-sign-out.component';
 import Header from './components/header/header-component';
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path ='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
-    </div>
+    </div>  
   );
 }
 

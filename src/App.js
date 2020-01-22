2f0b0dc5cba728bbc26e047c4129879e16ea6cc4
Erik Kers <erik.kers.ohrnell@gmail.com>
2020-01-22 15:26:21 +0100

    Subscribe to sign in

--- src/App.js
+++ src/App.js
@@ -5,18 +5,42 @@ import { Switch, Route} from 'react-router-dom';
 import ShopPage from './page/shop/shop.component';
 import SignInAndSignUp from './page/sign-in-and-sign-out/sign-in-and-sign-out.component';
 import Header from './components/header/header-component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path ='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
-      </Switch>
-    </div>  
-  );
+class App extends React.Component() {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path ='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>  
+    );
+  }
 }
 
 export default App;

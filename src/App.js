ae6741ea7345f79b80af99c13c5268059782f177
Erik Kers <erik.kers.ohrnell@gmail.com>
2020-01-31 16:05:19 +0100

    Setup saving users to db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route} from 'react-router-dom';
 import ShopPage from './page/shop/shop.component';
 import SignInAndSignUp from './page/sign-in-and-sign-out/sign-in-and-sign-out.component';
 import Header from './components/header/header-component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -20,8 +20,15 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     
+     if (userAuth) {
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot => this.setState({currentUser: {id: snapShot.id,...snapShot.data()} }));
+     } else {
+       this.setState({currentUser: userAuth})
+     }
+
     });
   }
 

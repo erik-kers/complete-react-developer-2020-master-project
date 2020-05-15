32026778dc173232e827981e3cac293197f04472
Erik Kers <erik.kers@innofactor.com>
2020-05-15 15:14:22 +0200

    Duplicate import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './index.css';
 
+import { persistor, store } from './redux/store';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import store from './redux/store';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>

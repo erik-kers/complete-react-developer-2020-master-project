10be7b2deb2b8775732f198ceeeb360344c6af50
Erik Kers <erik.kers@innofactor.com>
2020-05-15 15:13:52 +0200

    Add persistor to project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+import './index.css';
+
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import './index.css';
-import App from './App';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={ persistor }>
+        <App />
+      </PersistGate>
     </BrowserRouter>, 
   </Provider>,
   document.getElementById('root')
